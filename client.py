36c6b07517da17dedb999cdcf7e6a69a2155bbcb
MichaelE0211 <163016556+MichaelE0211@users.noreply.github.com>
2026-06-12 22:02:37 -0400

    Intial Client-Server

--- client.py
+++ client.py
@@ -1,3 +1,87 @@
 import socket
 import threading
-import sys
+
+def server_listener(data_socket, data_port  ):
+    while True:
+        try:
+            server_message = data_socket.recv(1024)
+            if not server_message:
+                # Server closed data channel
+                break
+
+            print(server_message.decode())
+        except Exception as e:
+            break
+            
+
+if __name__ == "__main__":
+    print("Starting client...")
+    control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    data_port = None
+    data_socket = None
+    server_ip = None
+
+    while True:
+        try:
+            user_input = input("> ").strip()
+        except (KeyboardInterrupt, EOFError):
+            break
+            
+        if not user_input:
+            continue
+
+        parts = user_input.split()
+        command = parts[0].lower()
+
+        match command:
+            case "connect":
+                if len(parts) < 3:
+                    print("Usage: connect <ip> <port>")
+                    continue
+
+                server_ip = parts[1]
+                control_port = int(parts[2])
+
+                try:
+                    control_socket.connect((server_ip, control_port))
+
+                    response = control_socket.recv(1024).decode()
+                    response_lines = response.split('\n')
+                    status_code = response_lines[0]
+
+                    if status_code == "200" and len(response_lines) > 1:
+                        data_port = int(response_lines[1])
+                        print(f"200 status coded received. Starting data connection on port {data_port}")
+
+                        data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        data_socket.connect((server_ip, data_port))
+                except Exception as e:
+                    print(e)
+                    control_socket = None
+            case "login":
+                if len(parts) < 2:
+                    print("Usage: login <username>")
+                    continue
+
+                try:
+                    control_socket.sendall(user_input.encode())
+
+                    response = data_socket.recv(1024).decode()
+                    if response == "200":
+                        print("200 status code received. Login successful")
+                    else:
+                        print("500 status code received. Failed to login")
+
+                    # Start thread to receive messages from server
+                    listening_thread = threading.Thread(
+                        target=server_listener,
+                        args=(data_socket, data_port)
+                    )
+
+                    listening_thread.daemon = True
+                    listening_thread.start()
+                except Exception as e:
+                    print(e)
+                    break
+
--- server.py
+++ server.py
@@ -1,3 +1,97 @@
+import random
 import socket
-import threading
 import sys
+import threading
+
+
+if len(sys.argv) < 2:
+    print("Error: Please include a port number to start the server")
+    sys.exit(1)
+
+PORT = int(sys.argv[1])
+
+active_connections = {}
+
+def handle_client_session(control_socket, data_port, client_address):
+    thread_id = threading.get_ident()
+    data_socket = None
+
+    data_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    data_listener.bind(('localhost', data_port))
+    data_listener.listen(1)
+
+    connection_response = f"200\n{data_port}"
+    print("Connection requested. Creating data socket")
+    control_socket.sendall(connection_response.encode())
+
+    try:
+        data_socket, data_address = data_listener.accept()
+    finally:
+        data_listener.close()
+
+    with control_socket:
+        while True:
+            try:
+                data = control_socket.recv(1024).decode()
+
+                parts = data.split()
+                if not parts:
+                    continue
+
+                command = parts[0].lower()
+
+                match command:
+                    case "login":
+                        username = parts[1]
+                        print(f"Login requestd by: {username}")
+
+                        login_response = "200"
+                        if username not in active_connections.keys():
+                            active_connections[username] = (data_socket)
+                        else:
+                            login_response = "500"
+
+                        data_socket.sendall(login_response.encode())
+
+                        # TODO: Broadcast login to all connected client
+                    case "who":
+                        pass
+                    case "broadcast":
+                        pass
+                    case "private":
+                        pass
+                    case "quit":
+                        pass
+
+            except ConnectionResetError:
+                pass
+
+def start_server():
+    print("Starting server")
+    print("Creating server socket")
+    
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind(('localhost', PORT))
+    serversocket.listen(1)
+
+    print("Awaiting connections...")
+
+    try:
+        while True:
+            control_socket, client_address = serversocket.accept()
+            data_port = random.randint(9000, 9100)
+
+            client_thread = threading.Thread(
+                target=handle_client_session,
+                args=(control_socket, data_port, client_address)
+            )
+
+            client_thread.daemon = True
+            client_thread.start()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        serversocket.close()
+
+if __name__ == "__main__":
+    start_server()
