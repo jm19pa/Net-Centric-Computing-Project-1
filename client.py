32ad8613cf4a2fb7a3955200f888bd86bddcb2fe
MichaelE0211 <michaelestrada0211@gmail.com>
2026-06-14 22:19:40 -0400

    Reworked Client

--- client.py
+++ client.py
@@ -1,19 +1,84 @@
 import socket
 import threading
 
-def server_listener(data_socket, data_port):
+response_ready = threading.Event()
+lock = threading.Lock()
+
+last_command = None
+last_response = None
+username = None
+
+def data_listener(data_socket, data_port):
     while True:
         try:
-            server_message = data_socket.recv(1024)
+            server_message = data_socket.recv(1024).decode()
             if not server_message:
-                # Server closed data channel
                 break
+            parts = server_message.split('\n')
 
-            message = server_message.decode()
-            print(message)
-        except Exception as e:
-            print(e)
-            
+            # Print messages from other users immediately
+            if len(parts) >= 5:
+                if parts[2] == "Broadcast":
+                    broadcast_message = " ".join(parts[4:])
+                    print(f"\nBroadcast message from {parts[3]}: {broadcast_message}", end="\n> ")
+                else:
+                    private_message = " ".join(parts[4:])
+                    print(f"\n{parts[3]}: {private_message}", end="\n> ")
+
+            # Otherwise for command response
+            elif len(parts) == 4 and parts[3] != username:
+                if parts[2] == "join":
+                    print(f"\n{parts[3]} has logged in.", end="\n> ")
+                else:
+                    print(f"\n{parts[3]} has logged out.", end="\n> ")
+            else:
+                with lock:
+                    global last_response
+                    last_response = server_message
+                    response_ready.set()
+
+        except:
+            break
+
+def print_response():
+    with lock:
+        global last_command, last_response
+        
+        if not last_response:
+            return
+
+        parts = last_response.split('\n')
+        status_code = parts[0]
+        
+        if last_command == "login":
+            if status_code == "200":
+                print("200 status code received. Login successful.")
+            else:
+                print("500 status code received. Failed to login.")
+        elif last_command == "who":
+            if status_code == "200":
+                users = parts[2]
+                print(f"200 status code received. Users currently connected: {users}")
+            else:
+                print(f"500 status code received. Failed to retrieve active users.")
+        elif last_command == "broadcast":
+            if status_code == "200":
+                print("200 status code received.")
+            else:
+                print("500 status code received. Failed to broadcast.")
+        elif last_command == "private":
+            if status_code == "200":
+                print("200 status code received. Message sent.")
+            else:
+                print("500 status code received. Message failed to send.")
+        elif last_command == "quit":
+            if status_code == "200":
+                print("200 status code received.")
+            else:
+                print("500 status code received. Failed to disconnect")
+
+        last_response = None
+
 
 if __name__ == "__main__":
     print("Starting client...")
@@ -27,6 +92,7 @@ if __name__ == "__main__":
         try:
             user_input = input("> ").strip()
         except (KeyboardInterrupt, EOFError):
+            print("Exiting")
             break
             
         if not user_input:
@@ -34,6 +100,10 @@ if __name__ == "__main__":
 
         parts = user_input.split()
         command = parts[0].lower()
+
+        if command not in ["connect", "login", "who", "broadcast", "private", "quit"]:
+            print("Invalid command.")
+            continue
 
         match command:
             case "connect":
@@ -51,60 +121,47 @@ if __name__ == "__main__":
                     response_lines = response.split('\n')
                     status_code = response_lines[0]
 
+                    # Force event to prevent yielding
+                    response_ready.set()
+
                     if status_code == "200" and len(response_lines) > 1:
                         data_port = int(response_lines[2])
                         print(f"200 status coded received. Starting data connection on port {data_port}")
 
                         data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                         data_socket.connect((server_ip, data_port))
+
+                         # Start thread to receive messages from server
+                        listening_thread = threading.Thread(
+                            target=data_listener,
+                            args=(data_socket, data_port)
+                        )
+                        listening_thread.daemon = True
+                        listening_thread.start()
                 except Exception as e:
                     print(e)
             case "login":
                 if len(parts) < 2:
                     print("Usage: login <username>")
                     continue
-
+                with lock:
+                    last_command = "login"
+                    username = parts[1]
                 try:
-                    # Send command to server for processing
                     control_socket.sendall(user_input.encode())
-
-                    response = data_socket.recv(1024).decode()
-                    if response == "200":
-                        print("200 status code received. Login successful")
-                    else:
-                        print("500 status code received. Failed to login")
-
-                    # Start thread to receive messages from server
-                    listening_thread = threading.Thread(
-                        target=server_listener,
-                        args=(data_socket, data_port)
-                    )
-                    listening_thread.daemon = True
-                    listening_thread.start()
                 except Exception as e:
                     print(e)
-                    break
             case "who":
+                last_command = "who"  
                 try:
                     control_socket.sendall(user_input.encode())
-
-                    response = data_socket.recv(1024).decode()
-                    response_lines = response.split('\n')
-                    status_code = response_lines[0]
-
-                    if status_code == "200" and len(response_lines) > 1:
-                        users = response_lines[1:]
-                        print("Active users:")
-                        for user in users:
-                            print(user)
-                    else:
-                        print("Failed to retrieve active users")
                 except Exception as e:
                     print(e)
             case "broadcast":
                 if len(parts) < 2:
                     print("Usage: broadcast <message>")
                     continue
+                last_command = "broadcast"
 
                 try:
                     control_socket.sendall(user_input.encode())
@@ -114,11 +171,27 @@ if __name__ == "__main__":
                 if len(parts) < 3:
                     print("Usage: private <username> <message>")
                     continue
-
+                last_command = "private"
                 try:
                     control_socket.sendall(user_input.encode())
                 except Exception as e:
                     print(e)
             case "quit":
+                last_command = "quit"
+                try:
+                    control_socket.sendall(user_input.encode())
+                except Exception as e:
+                    print(e)
+            
 
-                pass
+        # Wait for server command response before continuing
+        if command != "connect":
+            response_ready.wait()
+            response_ready.clear()
+            print_response()
+
+            if command == "quit":
+                if data_socket: data_socket.close()
+                if control_socket: control_socket.close()
+                print("Disconnected.")
+                break
--- server.py
+++ server.py
@@ -8,11 +8,9 @@ if len(sys.argv) < 2:
 
 PORT = int(sys.argv[1])
 
-# TODO: Active connections dictionary must be thread safe
 active_connections = {}
 active_connections_lock = threading.Lock()
 
-# TODO: Client->Server->Client communication functions
 def send_private_message(source_user, destination_user, message):
     with active_connections_lock:
         dest_socket = active_connections.get(destination_user)
@@ -20,7 +18,7 @@ def send_private_message(source_user, destination_user, message):
     if dest_socket is None:
         return False
 
-    outgoing = f"200\nPrivate\n{source_user}\n{message}".encode()
+    outgoing = f"200\n\nPrivate\n{source_user}\n{message}".encode()
     try:
         dest_socket.sendall(outgoing)
         return True
@@ -29,7 +27,27 @@ def send_private_message(source_user, destination_user, message):
 
 
 def broadcast_message_to_all(source_user, message):
-    outgoing = f"200\nBroadcast\n{source_user}\n{message}".encode()
+    outgoing = f"200\n\nBroadcast\n{source_user}\n{message}".encode()
+    with active_connections_lock:
+        sockets = list(active_connections.values())
+
+    for sock in sockets:
+        try:
+            sock.sendall(outgoing)
+        except Exception:
+            pass
+
+def broadcast_user_join(joined_user):
+    outgoing = f"200\n\njoin\n{joined_user}".encode()
+    with active_connections_lock:
+        for username in active_connections.keys():
+            if username == joined_user:
+                continue
+            else:
+                active_connections[username].sendall(outgoing)
+
+def broadcast_user_disconnect(exited_user):
+    outgoing = f"200\n\nquit\n{exited_user}".encode()
     with active_connections_lock:
         sockets = list(active_connections.values())
 
@@ -83,51 +101,58 @@ def handle_client_session(control_socket, client_address):
 
                 command = parts[0].lower()
 
-                # TODO: Finish commands
                 match command:
                     case "login":
                         username = parts[1]
                         print(f"Login requestd by: {username}")
 
-                        login_response = "200"
+                        login_code = "200"
                         with active_connections_lock:
                             if username not in active_connections:
                                 active_connections[username] = data_socket
                             else:
-                                login_response = "500"
+                                login_code = "500"
 
-                        data_socket.sendall(login_response.encode())                
+                        login_response = f"{login_code}\n\njoin\n{username}"
+                        data_socket.sendall(login_response.encode())
+                        broadcast_user_join(username)                
                     case "who":
+                        print("Who requested. Sending users.")
                         with active_connections_lock:
-                            users = "\n".join(active_connections.keys())
+                            users = ", ".join(active_connections.keys())
 
-                        response = f"200\n{users}"
+                        response = f"200\n\n{users}"
                         data_socket.sendall(response.encode())
                     case "broadcast":
                         message = " ".join(parts[1:]).strip()
+                        print(f"Broadcast requested by {username}\nMessage: {message}")
+
                         if not message:
-                            data_socket.sendall("500\nMissing message".encode())
+                            data_socket.sendall("500".encode())
                             continue
 
-                        broadcast_message_to_all(username or "UNKNOWN", message)
-                        # data_socket.sendall("200".encode())
+                        data_socket.sendall("200".encode())
+                        broadcast_message_to_all(username or "UNKNOWN", message) 
                     case "private":
                         if len(parts) < 3:
                             data_socket.sendall("500\nUsage: private <username> <message>".encode())
                             continue
 
                         destination_user = parts[1]
+                        print(f"Private message from {username} to {destination_user}")
+                        
                         message = " ".join(parts[2:]).strip()
                         if send_private_message(username or "SERVER", destination_user, message):
                             data_socket.sendall("200".encode())
                         else:
-                            data_socket.sendall("500\nUser not found or send failed".encode())
+                            data_socket.sendall("500".encode())
                     case "quit":
                         if username:
                             remove_connection(username)
                         data_socket.sendall("200".encode())
-                        break
-
+                        data_socket.close()
+                        broadcast_user_disconnect(username)
+                        return
             except ConnectionResetError:
                 if username:
                     remove_connection(username)
